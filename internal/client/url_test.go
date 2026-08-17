a55c50f2a713d92e999060c373e84b1063c9c03d
Chad E. Brizendine <xstrawman@disroot.org>
2026-08-17 13:51:37 -0600

    test: proxy URLs never touch ultimate-guitar.com

---
+++ internal/client/url_test.go
@@ -0,0 +1,39 @@
+package client
+
+import (
+	"strings"
+	"testing"
+)
+
+func TestSearchURL_usesProxyAndNeverUG(t *testing.T) {
+	u, err := SearchURL(DefaultSearchHost, "nelson always", 2)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if strings.Contains(u, "ultimate-guitar.com") {
+		t.Fatal(u)
+	}
+	if !strings.HasPrefix(u, "https://proxy.freetar.de/search.php?") {
+		t.Fatal(u)
+	}
+	if !strings.Contains(u, "search_type=title") || !strings.Contains(u, "page=2") {
+		t.Fatal(u)
+	}
+}
+
+func TestSearchURL_rejectsUGHost(t *testing.T) {
+	_, err := SearchURL("https://www.ultimate-guitar.com", "x", 1)
+	if err == nil {
+		t.Fatal("expected reject")
+	}
+}
+
+func TestTabURL_stripsPrefix(t *testing.T) {
+	u, err := TabURL(DefaultTabHost, "/tab/willie-nelson/always-on-my-mind-123")
+	if err != nil {
+		t.Fatal(err)
+	}
+	if u != "https://tabs.proxy.freetar.de/tab/willie-nelson/always-on-my-mind-123" {
+		t.Fatal(u)
+	}
+}
